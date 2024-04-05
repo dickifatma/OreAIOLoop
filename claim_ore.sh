7304e1f3fc0e0ca0639ecc76604bb7055811da42
dickifatma <116485777+dickifatma@users.noreply.github.com>
2024-04-05 09:46:55 +0700

    Update claim_ore.sh

--- claim_ore.sh
+++ claim_ore.sh
@@ -1,10 +1,13 @@
-#Infinite Loop
+# Infinite loop
 while true
 do
-  echo "Memulai Ore claim proses..."
-  ore --keypair ~/.config/solana/id.json claim
-
-  echo "Memulai ulang..."
-  # Opsional: melakukan delay sleep selama beberapa detik untuk menghindari spam
-  sleep 15
+  echo "Memulai proses klaim..."
+  ore \
+    --rpc https://mainnet.helius-rpc.com/?api-key=c882fbbb-d78e-4048-93b9-ba92a8fff07d \
+    --keypair ~/.config/solana/id.json \
+    --priority-fee 5555550 \
+    claim 0.024
+     echo "Proses klaim dimuat ulang..."
+  #  Opsional: melakukan delay sleep selama beberapa detik untuk menghindari spam
+  sleep 3
 done
