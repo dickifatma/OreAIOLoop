75f4f09ba65e9eac1694ea6c4883231d922ef59f
dickifatma <116485777+dickifatma@users.noreply.github.com>
2024-04-05 07:05:56 +0700

    Update mining_ore.sh

--- mining_ore.sh
+++ mining_ore.sh
@@ -3,9 +3,9 @@ while true
 do
   echo "Memulai proses menambang..."
   ore \
-    --rpc https://solana-mainnet.g.alchemy.com/v2/-pvIKEVWQkIKKRmEAHKXbEknf_c2nu9C \
+    --rpc https://sleek-shy-telescope.solana-mainnet.quiknode.pro/596d3c4655373d45b0ad337ca2f4b472a68fa96f/ \
     --keypair ~/.config/solana/id.json \
-    --priority-fee 5000000 \
+    --priority-fee 2220000 \
     mine \
     --threads 10
   echo "Proses mining dimuat ulang..."
